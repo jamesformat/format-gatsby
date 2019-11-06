5a9c4beaa3a27afb4543c9b3e7b1f6454fe589fa
Simon Bays <simon.bays@gmail.com>
2019-11-06 11:55:36 +0000

    remove reference to aboutUs page in cms.js

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,7 +2,6 @@ import CMS from "netlify-cms-app";
 import uploadcare from "netlify-cms-media-library-uploadcare";
 import cloudinary from "netlify-cms-media-library-cloudinary";
 
-import AboutPagePreview from "./preview-templates/AboutPagePreview";
 import BlogPostPreview from "./preview-templates/BlogPostPreview";
 import ProjectsPagePreview from "./preview-templates/ProjectsPagePreview";
 import IndexPagePreview from "./preview-templates/IndexPagePreview";
@@ -11,6 +10,5 @@ CMS.registerMediaLibrary(uploadcare);
 CMS.registerMediaLibrary(cloudinary);
 
 CMS.registerPreviewTemplate("index", IndexPagePreview);
-CMS.registerPreviewTemplate("about", AboutPagePreview);
 CMS.registerPreviewTemplate("projects", ProjectsPagePreview);
 CMS.registerPreviewTemplate("blog", BlogPostPreview);
